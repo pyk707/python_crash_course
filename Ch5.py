8428389eafb7d23dbeb2018e7259c545115aa466
pyk707 <pyk707@hanmail.net>
2020-12-16 21:47:11 +0900

    12/16 update

--- Ch5.py
+++ Ch5.py
@@ -1,3 +1,4 @@
+""" ##if 사용
 cars = ['audi', 'bmw', 'subaru', 'toyota']
 for car in cars:
     if car == 'bmw':
@@ -5,9 +6,37 @@ for car in cars:
     else:
         print(car.title())
 
+print()
 
-# and와 or
+
+## and와 or
 a = 10
 b = 20
 
 print(a > 5 and b >= 10)
+print() """
+
+
+"""##리스트 안의 값 찾기
+ requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+
+print("\nFinished making your pizza!")
+print() """
+
+print()
+available_toppings = ['mushrooms', 'olives', 'green peppers','pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
+
+print("\nFinished making your pizza!")
+print()
