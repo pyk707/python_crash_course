c16448da06467b77bf02817296dede46b168b807
pyk707 <pyk707@hanmail.net>
2020-12-16 21:17:08 +0900

    12/16 update

--- Ch4.py
+++ Ch4.py
@@ -4,3 +4,5 @@ magicians = ['alice', 'david', 'carolina']
 
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
+
+print(magicians[0]) # 다음 코드와 같음 -> print(magicians[0:1])
